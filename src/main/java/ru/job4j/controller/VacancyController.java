364b55809038e48778105e82f36df4ca1515921e
iiuhenio <iuheniomail@yandex.ru>
2023-07-03 18:51:33 +0300

    10. Обновление кода без перезагрузки сервера. [#504846 #427202]

--- src/main/java/ru/job4j/controller/VacancyController.java
+++ src/main/java/ru/job4j/controller/VacancyController.java
@@ -3,9 +3,15 @@ package ru.job4j.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.model.Vacancy;
 import ru.job4j.repository.MemoryVacancyRepository;
 import ru.job4j.repository.VacancyRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/vacancies")
@@ -22,6 +28,12 @@ public class VacancyController {
     @GetMapping("/create")
     public String getCreationPage() {
         return "vacancies/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Vacancy vacancy) {
+        vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
     }
 
 }
